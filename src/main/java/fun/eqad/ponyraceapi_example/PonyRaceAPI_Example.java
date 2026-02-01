8edeca4a773b6cae1d08e44dc8744d867426ca63
JessDaodao <694428809@qq.com>
2026-02-02 01:11:44 +0800

    更新

--- src/main/java/fun/eqad/ponyraceapi_example/PonyRaceAPI_Example.java
+++ src/main/java/fun/eqad/ponyraceapi_example/PonyRaceAPI_Example.java
@@ -9,7 +9,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class PonyRace_Example extends JavaPlugin {
+public class PonyRaceAPI_Example extends JavaPlugin {
 
     private PonyRaceAPI ponyRaceAPI;
 
